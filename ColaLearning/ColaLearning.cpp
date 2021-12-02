1d9dd7f6184c9ac91f8e7236d6a0bb2bc973d7b2
SaeGot <saegot@naver.com>
2021-12-02 18:26:02 +0900

    XOR 게이트 추가, NeuralNetwork docstring 추가

--- ColaLearning/ColaLearning.cpp
+++ ColaLearning/ColaLearning.cpp
@@ -70,7 +70,7 @@ int main()
 		}
 		printf_s("\n");
 	}
-	/*
+	
 	// XOR 게이트
 	printf_s("[XOR 게이트] \n");
 	vector<vector<double>>::iterator iter_input;
@@ -82,28 +82,17 @@ int main()
 		Layer layer_hidden(2);
 		Layer layer_output(1);
 		vector<Layer> layers({ layer_input, layer_hidden, layer_output });
-		// 각 게이트 출력 계산
-		vector<vector<double>> vec_weight1;
+		// 각 가중치 생성
+		vector<vector<double>> vec_weight;
 		for (int i = 0; i < 3; i++)
 		{
-			vec_weight1[0].push_back(weight_or[i][0]);
-			vec_weight1[1].push_back(weight_nand[i][0]);
+			vec_weight.push_back({ weight_or[i][0], weight_nand[i][0] });
 		}
-		vector<Weight> weights({ weight });
-		double gate_or_sum = Sum(layer1, weight_list.at(OR));
-		double gate_nand_sum = Sum(layer1, weight_list.at(NAND));
-		double gate_or_output = Activate(gate_or_sum);
-		double gate_nand_output = Activate(gate_nand_sum);
-		// 두 번째 층 입력
-		vector<double> layer2_input;
-		layer2_input.push_back(gate_or_output);
-		layer2_input.push_back(gate_nand_output);
-		Layer layer2(layer2_input);
-		// 두 번째 층 출력 계산
-		output_sum = Sum(layer2, weight_list.at(AND));
-		output_activated = Activate(output_sum);
-		printf_s("입력 : %1lf, %1lf → 출력 : %lf\n", input[0], input[1], output_activated);
+		vector<Weight> weights({ vec_weight, weight_list.at(Gate::AND) });
+		// 신경망 생성
+		NeuralNetwork net(layers, weights);
+		printf_s("입력 : %1lf, %1lf → 출력 : %lf\n", input[0], input[1], net.Predict()[0]);
 	}
 	printf_s("\n");	
-	*/
+	
 }
--- ColaLearning/NeuralNetwork.h
+++ ColaLearning/NeuralNetwork.h
@@ -10,17 +10,51 @@ using namespace std;
 class NeuralNetwork
 {
 public:
+	/**
+	 * Layer 만으로 NeuralNetwork 생성.
+	 * 
+	 * \param _layers
+	 */
 	NeuralNetwork(vector<Layer> _layers);
+	/**
+	 * Layer와 Weight로 NeuralNetwork 생성.
+	 * 
+	 * \param _layers
+	 * \param _weights
+	 */
 	NeuralNetwork(vector<Layer> _layers, vector<Weight> _weights);
 	~NeuralNetwork() {};
 
+	/**
+	 * 예측 값들 계산.
+	 * 
+	 * \return 예측 값들
+	 */
 	vector<double> Predict();
 
 private:
 	vector<Layer> layers;
 	vector<Weight> weights;
 
+	/**
+	 * 모든 가중치 초기화.
+	 * 
+	 */
 	void InitWeights();
+	/**
+	 * 활성화 이전의 노드 값 계산.
+	 * 
+	 * \param layer : 이전 Layer
+	 * \param weight : 이전 Layer와 다음 Layer 사이의 가중치
+	 * \param j : 다음 Layer의 노드 인덱스
+	 * \return 다음 노드 j 번째 값
+	 */
 	double Sum(const Layer &layer, const Weight &weight, int j);
+	/**
+	 * 활성.
+	 * 
+	 * \param value : 활성 대상 값
+	 * \return 활성 값 
+	 */
 	double Activate(double value);
 };
