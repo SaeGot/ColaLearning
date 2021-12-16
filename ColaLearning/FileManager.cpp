0cd70a395b2250f1bd3c8ec1513a19f3ed813de8
SaeGot <saegot@naver.com>
2021-12-16 22:46:42 +0900

    파일매니저 tsv 읽기

--- ColaLearning/FileManager.cpp
+++ ColaLearning/FileManager.cpp
@@ -5,11 +5,22 @@ FileManager::FileManager(string file_Name)
 {
 	fstream file;
 	file.open(file_Name);
-	string str_data;
 
-	while (!file.eof())
+	int row = 0;
+	int col;
+	string line;
+	while (getline(file, line))
 	{
-		getline(file, str_data, '\t');
-
+		col = 0;
+		vector<double> line_data;
+		stringstream ss_line;
+		string str_data;
+		while (getline(ss_line, str_data, '\t'))
+		{
+			line_data.push_back(stod(str_data));
+			col++;
+		}
+		data.push_back(line_data);
+		row++;
 	}
 }
--- ColaLearning/FileManager.h
+++ ColaLearning/FileManager.h
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <sstream>
 
 using namespace std;
 
