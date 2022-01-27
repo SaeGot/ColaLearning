d165b4623f468fa47a1c22a02d8e9535ea5547e5
SaeGot <saegot@naver.com>
2022-01-27 14:15:15 +0900

    Q-L:earning 설계중

--- ColaLearning/QLearning.h
+++ ColaLearning/QLearning.h
@@ -1,7 +1,23 @@
 #pragma once
 #include "ReinforcementLearning.h"
+#include <map>
+#include <string>
 
 
 class QLearning : public ReinforcementLearning
 {
+public:
+	void Learn();
+	vector<string> GetBest();
+
+protected:
+	void TryStep();
+
+private:
+	// ToDo 각 상태별 행동에 대해 어떤 상태가 되는지에 대한 표
+
+	// i : 에피소드, j : 상태
+	vector<vector<string>> states;
+	// i : 에피소드, j : 행동
+	vector<vector<string>> action;
 };
