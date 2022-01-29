4cba5b5d390fe61ca20509357051368c4056fe0e
SaeGot <saegot@naver.com>
2022-01-29 21:20:06 +0900

    QLearning 설계중...

--- ColaLearning/QLearning.h
+++ ColaLearning/QLearning.h
@@ -7,6 +7,12 @@
 class QLearning : public ReinforcementLearning
 {
 public:
+	/**
+	 * csv 파일로 상태별 행동에 따른 다음 상태 표 생성.
+	 * 
+	 * \param file_Name : 파일명
+	 */
+	QLearning(string file_Name);
 	void Learn();
 	vector<string> GetBest();
 
@@ -14,7 +20,13 @@ protected:
 	void TryStep();
 
 private:
+	struct StateAction
+	{
+		string state;
+		string action;
+	};
 	// ToDo 각 상태별 행동에 대해 어떤 상태가 되는지에 대한 표
+	map<StateAction, string> nextState;
 
 	// i : 에피소드, j : 상태
 	vector<vector<string>> states;
