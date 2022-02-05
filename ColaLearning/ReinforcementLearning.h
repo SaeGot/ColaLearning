3b5341a9a89c330f2225f44487c7ec00fcb420e9
SaeGot <saegot@naver.com>
2022-02-05 23:47:13 +0900

    ReinforcementLearning epsilon-greedy 설계중

--- ColaLearning/ReinforcementLearning.h
+++ ColaLearning/ReinforcementLearning.h
@@ -11,6 +11,16 @@ public:
 		State,
 		Reward
 	};
+	struct epsilonGreedy
+	{
+		float startValue;
+		// true : 스텝마다 감가율 적용, false : 에피소드마다 감가율 적용
+		bool discountPerStep;
+		// 몇 interval마다 스텝 혹은 에피소드마다 감가율 적용하는지 결정. 0 이하는 1로 적용
+		int interval;
+		// 감가율
+		float gamma;
+	};
 
 protected:
 	struct SARS
