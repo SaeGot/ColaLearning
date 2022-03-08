98547e75575ebefe0be11d1c90bde07451ede90c
SaeGot <saegot@naver.com>
2022-03-08 18:27:04 +0900

    Normalize 설계중

--- ColaLearning/Layer.h
+++ ColaLearning/Layer.h
@@ -107,8 +107,14 @@ public:
 	ActivationFunction GetActivationFunction() const;
 
 private:
+	struct MinMax
+	{
+		double min;
+		double max;
+	};
 	vector<double> nodeValues;
 	vector<double> backNodeValues;
+	vector<MinMax> nodeMinMax;
 	ActivationFunction activationFunction;
 	bool bias;
 };
